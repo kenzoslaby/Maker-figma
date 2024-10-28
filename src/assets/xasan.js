43eda08b916f7738dd9d3d1029cd60ed4e4575be
XasanboyKarimov <xkarimov257@gmail.com>
2024-10-28 17:09:46 +0500

    Figma end

---
+++ src/assets/xasan.js
@@ -0,0 +1,41 @@
+import crafted from './icons/crafted.svg';
+import sleek from './icons/sleek.svg';
+import time from './icons/time.svg';
+import logo2 from './icons/LogoInpus.svg';
+import coovid from './icons/Covid19.svg';
+
+
+const recall = [
+    {
+        img: sleek,
+        title: "Sleek and smooth animation",
+        description: "Powerful creations for the practice safe design creations for the prac."
+    },
+    {
+        img: crafted,
+        title: "Crafted with detail",
+        description : "Powerful creations for the practice safe design creations for the prac."
+    },
+    {
+        img: time,
+        title: "Save your time",
+        description: "Powerful creations for the practice safe design creations for the prac."
+    },
+    {
+        img: sleek,
+        title: "Sleek and smooth animation",
+        description: "Powerful creations for the practice safe design creations for the prac."
+    },
+    {
+        img: crafted,
+        title: "Crafted with detail",
+        description : "Powerful creations for the practice safe design creations for the prac."
+    },
+    {
+        img: time,
+        title: "Save your time",
+        description: "Powerful creations for the practice safe design creations for the prac."
+    },
+]
+
+export { recall, logo2, coovid }

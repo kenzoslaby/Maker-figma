60701212ed503ebe442d75edd42ddc536cd625c7
kenzoslaby <kenzoslaby@gmail.com>
2024-10-28 14:56:38 +0500

    Maker figma

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      maxWidth:{
+        "1180px": "1180px", 
+      },
+      colors:{
+        "blue": "#35afbf",
+        "darkblue": "#2c353e",
+        "gray": "#707273",
+        "black": "#252626"
+      },
+    },
+  },
+  plugins: [],
+}
